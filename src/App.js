059db63f151e76898545bae8f90dbbe60cec040d
Mitchell Knox <mitchk612@yahoo.com>
2020-11-05 15:06:39 -0600

    google analytics initialization

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+
+const trackingId = 'G-VPM46C1Z1R';
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.initialize(trackingId)
+    ReactGA.pageview(window.location.pathname)
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
