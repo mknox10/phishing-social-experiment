13306cc63fd6dff1c48a650f8d6bd0b53d54511d
Mitchell Knox <mitchk612@yahoo.com>
2020-11-12 14:58:16 -0600

    styled login page to replicate campus connection

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,6 +2,7 @@ import { Layout} from 'antd';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+import logo from '../NDUS_Logo.png';
 import '../index.css';
 
 const { Footer } = Layout;
@@ -15,24 +16,39 @@ function Login() {
     }
 
     return (
-        <div>
-            <div class="col-xs-1" align="center" style={{margin: 340}}>
-                <div class="card" style={{width: 240}}>
+        <div align="center">
+            <div id="NDSU_logo" style={{margin: 20}}>
+                <img src={logo} />
+            </div>
+            <div class="col-xs-1">
+                <div class="card">
                     <div id="login" class="card-body">
                         <form>
-                            <h5>Campus Connection Log In</h5>
-                            <input placeholder="email" type="email" />
+                            <h4 align="left">Campus Connection Log In</h4>
+                            <p align="left">NDUS Campus Connection requires authentication using your NDUS identifier.</p>
+                            <h6 align="left">User ID</h6>
+                            <input placeholder="User ID" />
+                            <h6 align="left">Password</h6>
                             <input placeholder="password" type="password" />
-                            <button onClick={redirect}>Login</button>
-                            <div>
-                                <a href="#" onClick={redirect}>Forgot Password?</a>
-                            </div>
+                            <p>I agree to the terms of the 
+                                <a href="#" onClick={redirect}>
+                                     User Agreement
+                                </a>
+                            </p>
+                            <button type="button" class="btn btn-light" onClick={redirect}>Log In</button>
+                            <h4 align="left">Trouble Logging In?</h4>
+                            <p align="left">If you are experiencing any difficulties getting logged into the system, please use the links below.</p>
+                            <button type="button" class="btn btn-info" onClick={redirect}>NDUS System Status</button>
+                            <button type="button" class="btn btn-info" onClick={redirect}>User ID and Password Help</button>
+                            <button type="button" class="btn btn-info" onClick={redirect}>Send email to Help Desk</button>
+                            <button type="button" class="btn btn-info" onClick={redirect}>Guest Page</button>
                         </form>
                     </div>
                 </div>
             </div>
-            <Footer style={{ textAlign: 'center' }}>
-                North Dakota University System. 2020.
+            <hr></hr>
+            <Footer>
+                <p class="my-footer">2020 North Dakota University System.</p>
             </Footer>
         </div>
     )
